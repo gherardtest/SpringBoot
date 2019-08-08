87c3e14f12175ad9c28bcefaa997925000e9385d
Gherard Chipana <gchipana@everis.com>
2019-08-08 18:37:31 -0500

    MVC

---
+++ DemoWeb/src/main/java/com/gherard/rest/RestDemoController.java
@@ -0,0 +1,15 @@
+package com.gherard.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestDemoController {
+
+	//private IPersona repo;
+	
+	//@GetMapping
+	//public List<Persona> listar(){
+		
+	}
+//}
