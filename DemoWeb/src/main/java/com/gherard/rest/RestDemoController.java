43ffb0638e2cb239dd20e7480d2110f05e65e8b5
Gherard Chipana <gchipana@everis.com>
2019-08-09 18:17:33 -0500

    MiniCRUD

--- DemoWeb/src/main/java/com/gherard/rest/RestDemoController.java
+++ DemoWeb/src/main/java/com/gherard/rest/RestDemoController.java
@@ -3,13 +3,20 @@ package com.gherard.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gherard.model.Persona;
 import com.gherard.repo.IPersonaRepo;
 
 @RestController
+@RequestMapping("/personas")
 public class RestDemoController {
 
 	@Autowired
@@ -19,4 +26,20 @@ public class RestDemoController {
 	public List<Persona> listar(){
 		return repo.findAll();
 	}
+	
+	@PostMapping
+	public void insertar(@RequestBody Persona per){
+		repo.save(per);
+	}
+	
+	@PutMapping
+	public void modificar(@RequestBody Persona per){
+		repo.save(per);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void eliminar(@PathVariable("id") Integer id){
+		repo.deleteById(id);
+	}
+	
 }
